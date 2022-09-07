59ae4134197bc5ad8958ce44d8c660d0ac3c769e
LiMi-SEDC <limimisini@hotmail.com>
2022-09-07 19:16:29 +0200

    channelDetail done

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -4,7 +4,7 @@ import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import { Link } from 'react-router-dom';
 import { demoProfilePicture } from '../utils/constants';
 
-const ChannelCard = ({ channelDetail }) => (
+const ChannelCard = ({ channelDetail, marginTop }) => (
   <Box
     sx={{
       boxShadow: 'none',
@@ -15,6 +15,7 @@ const ChannelCard = ({ channelDetail }) => (
       width: { xs: '356px', md: '320px' },
       height: '326px',
       margin: 'auto',
+      marginTop,
     }}
   >
     <Link to={`/channel/${channelDetail?.id?.channelId}`}>
@@ -41,16 +42,16 @@ const ChannelCard = ({ channelDetail }) => (
           }}
         />
         <Typography variant="h6">
-          {channelDetail?.snippet?.title}{' '}
+          {channelDetail?.snippet?.title}
           <CheckCircleIcon
             sx={{ fontSize: '14px', color: 'gray', ml: '5px' }}
           />
         </Typography>
         {channelDetail?.statistics?.subscriberCount && (
-          <Typography>
+          <Typography sx={{ fontSize: '15px', fontWeight: 500, color: 'gray' }}>
             {parseInt(
               channelDetail?.statistics?.subscriberCount
-            ).toLocaleString()}
+            ).toLocaleString('en-US')}{' '}
             Subscribers
           </Typography>
         )}
