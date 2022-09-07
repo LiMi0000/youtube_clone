f69a44d0b9ea87aa0feb4c4d7dcf09b16bb3b71c
LiMi-SEDC <limimisini@hotmail.com>
2022-09-07 20:07:41 +0200

    search bar functionality done

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -33,7 +33,7 @@ const VideoCard = ({
           sx={{ width: 358, height: 180 }}
         />
       </Link>
-      <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }}>
+      <CardContent sx={{ backgroundColor: '#000', height: '106px' }}>
         <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
           <Typography variant="subtitle1" fontWeight="bold" color="#fff">
             {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
